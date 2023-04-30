2b21ac4e82f9a79d750762b6d9486454065a533f
JuanAyala <juanpabloayalaarboleda@gmail.com>
2023-04-29 19:50:47 -0500

    modificacion de comentarios 2

--- Comandos.py
+++ Comandos.py
@@ -9,5 +9,10 @@
 # git status => estado de mi rama
 # git add => me perite agregar archivos a la rama
 # git commit -m "comentario"  => me permite lanzar los cambios a algo real
-git log => verificar si se ha hecho el commit
+# git log => historial de los commits que se han hecho
+# git checkout => devuelve todos los cambios hasta el ultimo commit
+# git reset => me permite saber cuales archivos no se han guardado
+# git config --global alias.nombredelalias "configuracion" => me permite almacenar comandos personalizados
+# .gitignore => ficheros que no queremos tener en cuenta (**/. archivo)
+
 """
