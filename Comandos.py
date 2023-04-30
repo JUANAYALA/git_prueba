c6a545da44344decfa9ca1a5182c4aa5aba90f90
JuanAyala <juanpabloayalaarboleda@gmail.com>
2023-04-29 19:34:17 -0500

    modificacion de comentarios

--- Comandos.py
+++ Comandos.py
@@ -3,9 +3,11 @@
 # git -h => Ayudas para git
 # git config --global user.name "nombre del usuario"
 # git config --global user.mail "correo electronico"
-# git init me permite inicializar git
+# git init => me permite inicializar git
 # Ramas es un conjunto de archivos almacenados en un lugar especifico
-# git branch -m nombre de la rama
+# git branch -m nombre de la rama => crear una rama
 # git status => estado de mi rama
 # git add => me perite agregar archivos a la rama
+# git commit -m "comentario"  => me permite lanzar los cambios a algo real
+git log => verificar si se ha hecho el commit
 """
