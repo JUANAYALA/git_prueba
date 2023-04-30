7354a718bc938ec760dfeaf019defffb845156af
JuanAyala <juanpabloayalaarboleda@gmail.com>
2023-04-29 19:28:25 -0500

    mi primer commit

---
+++ Comandos.py
@@ -0,0 +1,11 @@
+"""
+# mkdir => crear una carpeta
+# git -h => Ayudas para git
+# git config --global user.name "nombre del usuario"
+# git config --global user.mail "correo electronico"
+# git init me permite inicializar git
+# Ramas es un conjunto de archivos almacenados en un lugar especifico
+# git branch -m nombre de la rama
+# git status => estado de mi rama
+# git add => me perite agregar archivos a la rama
+"""
