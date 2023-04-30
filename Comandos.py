4bd685cef47a0aa950cb7fcb9290ca50854f7fcb
JuanAyala <juanpabloayalaarboleda@gmail.com>
2023-04-30 16:26:33 -0500

    modificacion de comentarios 7

--- Comandos.py
+++ Comandos.py
@@ -1,4 +1,5 @@
 """
+//*****************************************   PRIMERA PARTE  *******************************************
 # mkdir => crear una carpeta
 # git -h => Ayudas para git
 # git config --global user.name "nombre del usuario"
@@ -11,6 +12,7 @@
 # git commit -m "comentario"  => me permite lanzar los cambios a algo real
 # git log => historial de los commits que se han hecho
 # git checkout => situarnos en un punto concreto del ultimo commit
+# git checkout main => situarnos en el main
 # git reset => me permite saber cuales archivos no se han guardado
 # git config --global alias.nombredelalias "configuracion" => me permite almacenar comandos personalizados
 # .gitignore => ficheros que no queremos tener en cuenta (**/. nombrearchivo)
@@ -19,7 +21,13 @@
 # git nombredelalias
 # git reset --hard iddelhashdonde me quiero situar => elimina los commit despues de cierto punto
 # git reflog => me permite ver el historial de todo los commit eliminados
+
+//*****************************************   SEGUNDA PARTE  *******************************************
 # git tag nombre=> crea una etiqueta de lo que representa una version
+# git checkout tags/nombredeltag
+# git branch nombrerama => crear una nueva rama para probar funcionalidades sin afectar la de produccion
+git switch nombrerama => moverse entre las ramas
+git merge nombredelarama => me permite actualizar los cambios de una rama a otra
 """
 
 
