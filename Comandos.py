c8de7c1db5e0d39a6c4893c9a5556d8d5e6e5b51
JuanAyala <juanpabloayalaarboleda@gmail.com>
2023-04-30 14:35:30 -0500

    modificacion de comentarios 3

--- Comandos.py
+++ Comandos.py
@@ -5,14 +5,18 @@
 # git config --global user.mail "correo electronico"
 # git init => me permite inicializar git
 # Ramas es un conjunto de archivos almacenados en un lugar especifico
-# git branch -m nombre de la rama => crear una rama
-# git status => estado de mi rama
+# git branch -m nombre de la rama => renombrar la rama en la que estamos
+# git status => estado de las ramas
 # git add => me perite agregar archivos a la rama
 # git commit -m "comentario"  => me permite lanzar los cambios a algo real
 # git log => historial de los commits que se han hecho
 # git checkout => devuelve todos los cambios hasta el ultimo commit
 # git reset => me permite saber cuales archivos no se han guardado
 # git config --global alias.nombredelalias "configuracion" => me permite almacenar comandos personalizados
-# .gitignore => ficheros que no queremos tener en cuenta (**/. archivo)
+# .gitignore => ficheros que no queremos tener en cuenta (**/. nombrearchivo)
+# git diff => mirar los cambios que se han hecho respecto al ultimo commit
+# git checkout numero del hash => me permite moverme en los diferentes commits realizados
 
 """
+
+
